1d75b5fec4fac38f25f9168ea1e16e2a9d375232
Wendel <wendelfelius@gmail.com>
2025-07-25 12:49:20 +0200

    Update const.py

--- custom_components/previous_state_tracker/const.py
+++ custom_components/previous_state_tracker/const.py
@@ -1,3 +1,5 @@
 DOMAIN = "previous_state_tracker"
-CONF_ENTITIES = "entities"
-CONF_NAME = "name"
+PLATFORMS = ["sensor"]
+CONF_ENTITY_ID = "entity_id"
+CONF_IGNORE_UNKNOWN = "ignore_unknown"
+CONF_IGNORE_UNAVAILABLE = "ignore_unavailable"
